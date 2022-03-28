a57215a67abfdced65bd8553022fcc7cf82935f7
federico <romerof14@gmail.com>
2022-03-28 12:00:44 -0300

    Agregados mas concepetos.

--- main.php
+++ main.php
@@ -27,6 +27,11 @@
 				<h3 class = "btn" id = "btn_min-max">MIN-MAX</h3>
 				<h3 class = "btn" id = "btn_count-sum-avg">COUNT-SUM-AVG</h3>
 				<h3 class = "btn" id = "btn_like">LIKE</h3>
+				<h3 class = "btn" id = "btn_in">IN</h3>
+				<h3 class = "btn" id = "btn_between">BETWEEN</h3>
+				<h3 class = "btn" id = "btn_as">AS</h3>
+				<h3 class = "btn" id = "btn_join">JOIN</h3>
+				<h3 class = "btn" id = "btn_union">UNION</h3>
 			</div>
 
 			<div id = "divBody" class =  "">
@@ -96,7 +101,7 @@
 						<div class = "centrar fila">
 							<b class = "colorComando"><=</b>Menor o igual a</div>
 						<div class = "centrar fila">
-							<b class = "colorComando">!=</b>Distinto</div>
+							<b class = "colorComando">!=</b>Distinto (<>)</div>
 						<div class = "centrar fila">
 							<b class = "colorComando">BETWEEN</b>Entre el rango descripto</div>
 						<div class = "centrar fila">
@@ -164,6 +169,15 @@
 				<b class = "colorComando">WHERE</b> condition; <b class = "colorComentario"> # Elimina todos los registros (fila) que cumplan la condicion. Si es omitida la clausula 'WHERE', se modificaran todos los registros.</b>
 				<br><br>
 				<b class = "colorComando">DELETE FROM</b> table_name;<b class = "colorComentario"> # Si se elimina toda la tabla, solo lo haran sus datos, no la estructura.</b>
+				<br><br>
+				<hr>
+				<br>
+				<b class = "colorImportante indice" id = "info_limit">LIMIT:</b><i> Especifica el numero de registro a mostrar.</i>
+				<br><br>
+				<b class = "colorComando">SELECT</b> column_name<br>
+				<b class = "colorComando">FROM</b> table_name<br>
+				<b class = "colorComando">WHERE</b> condition<br>
+				<b class = "colorComando">LIMIT</b> number;
 				<br><br>
 				<hr>
 				<br>
@@ -267,6 +281,112 @@
 				<b class = "colorComando">SELECT</b> column_name<br>
 				<b class = "colorComando">FROM</b> table_name<br>
 				<b class = "colorComando">WHERE</b> column_name NOT LIKE <b class = "colorString">'%a'</b>; <b class = "colorComentario"> # Filtra los resultados que no terminen en 'a'.</b>
+				<br><br>
+				<hr>
+				<br>
+				<b class = "colorImportante indice" id = "info_in">IN:</b><i> Utilizado en la clausula 'WHERE', permite especificar multiples valores. Es una abreviacion de multiples condiciones 'OR'.</i><br>
+				<br>
+				<b class = "colorComando">SELECT </b> column_name1<br>
+				<b class = "colorComando">FROM</b> table_name<br>
+				<b class = "colorComando">WHERE</b> column_name1 <b class = "colorComando">IN</b> (value1, value2); <b class = "colorComentario"> # Filtra si coincide con alguno de los valores especificados.</b>
+				<br><br>
+				<b class = "colorComando">SELECT </b> column_name1<br>
+				<b class = "colorComando">FROM</b> table_name<br>
+				<b class = "colorComando">WHERE</b> column_name1 <b class = "colorComando">NOT IN</b> (value1, value2); <b class = "colorComentario"> # Filtra si no coincide con alguno de los valores especificados.</b>
+				<br><br>
+				<b class = "colorComando">SELECT </b> column_name1<b class = "colorComentario"> # Tambien se puede utilizar otro 'SELECT' para filtrar.</b><br>
+				<b class = "colorComando">FROM</b> table_name1<br>
+				<b class = "colorComando">WHERE</b> column_name1 <b class = "colorComando">IN</b> <b class = "colorComentario"> # Filtra si coincide con algunos de los valores obtenidos del 'select' indicado.</b>
+				(<b class = "colorComando">SELECT </b> column_name2 <b class = "colorComando">FROM</b> table_name2);
+				<br><br>
+				<hr>
+				<br>
+				<b class = "colorImportante indice" id = "info_between">BETWEEN:</b><i> Utilizado en la clausula 'WHERE', filtra los valores que se encuentren entre los valores (numericos, cadenas y fechas) especificados. Es inclusivo en ambos extremos.</i><br>
+				<br>
+				<b class = "colorComando">SELECT </b> column_name1<br>
+				<b class = "colorComando">FROM</b> table_name<br>
+				<b class = "colorComando">WHERE</b> column_name1 <b class = "colorComando">BETWEEN</b> value1 <b class = "colorComando">AND</b> value2; <b class = "colorComentario"> # Filtra si el valor esta incluido en el rango.</b>
+				<br><br>
+				<b class = "colorComando">SELECT </b> column_name1<br>
+				<b class = "colorComando">FROM</b> table_name<br>
+				<b class = "colorComando">WHERE</b> column_name1 <b class = "colorComando">NOT BETWEEN</b> value1 <b class = "colorComando">AND</b> value2; <b class = "colorComentario"> # Filtra si el valor no esta incluido en el rango.</b>
+				<br><br>
+				<hr>
+				<br>
+				<b class = "colorImportante indice" id = "info_as">AS:</b><i> Es un alias que se puede utilizar para otorgarle un nombre temporal a la columna, fila o tabla indicada. El mismo solo dura hasta que termine la consulta. Es util para facilitar la lecura y trabajar mas comodo con multiples tablas.</i><br>
+				<br>
+				<b class = "colorComando">SELECT </b> column_name1 <b class = "colorComando">AS</b> C1, column_name2 <b class = "colorComando">AS</b> (C_2)<br>
+				<b class = "colorComando">FROM</b> table_name; <b class = "colorComentario"> # Cambio el nombre temporal de varias columnas. Si el mismo contiene espacios, se requiere el uso de parentesis.</b>
+				<br><br>
+				<b class = "colorComando">SELECT </b> column_name1<br>
+				<b class = "colorComando">FROM</b> table_name <b class = "colorComando">AS</b> TB1;<b class = "colorComentario"> # Cambio el nombre temporal de la tabla.</b>
+				<br><br>
+				<b class = "colorComando">SELECT CONCAT</b>(column_name1, <i class = "colorString">" "</i>, column_name2) <b class = "colorComando">AS</b> SUMA<br>
+				<b class = "colorComando">FROM</b> table_name; <b class = "colorComentario"> # 'CONCAT' permite concatenar los valores de las columnas indicadas, y a su vez cadenas que permiten separar los valores. Se puede indicar un alias para el resultado.</b>
+				<br><br>
+				<hr>
+				<br>
+				<b class = "colorImportante indice" id = "info_join">JOIN:</b><i> La clausula 'JOIN' permite combinar filas de dos o mas tablas, basadose la combinacion en una columna relacionada entre las mismas. El campo no necesariamente debe tener el mismo nombre en ambas tablas, solo el valor.<br>
+				En los casos de 'OUTER JOIN', si el campo no es coincidente, se rellena con nulo.</i><br>
+				<br>
+				<div class = "centrar vertical tabla-larga">
+					<div class = "centrar fila">
+						<b class = "colorImportante">TIPO DE 'JOIN' () ES OPCIONAL</b> <b class = "colorImportante">MUESTRA...</b>
+					</div>
+					<div class = "centrar fila">
+						<i><b class = "colorComando">(INNER) JOIN</b></i>
+						<i class = "descripcion">solo las filas que contengan el mismo valor relacionado.</i>
+					</div>
+					<div class = "centrar fila">
+						<i><b class = "colorComando">LEFT (OUTER) JOIN</b></i>
+						<i class = "descripcion">todas las filas de la tabla de la IZQ, y solo los coincidientes de la DER.</i>
+					</div>
+					<div class = "centrar fila">
+						<i><b class = "colorComando">RIGHT (OUTER) JOIN</b></i>
+						<i class = "descripcion">todas las filas de la tabla de la DER, y solo los coincidientes de la IZQ.</i>
+					</div>
+					<div class = "centrar fila">
+						<i><b class = "colorComando">FULL (OUTER) JOIN</b></i>
+						<i class = "descripcion">muestra todas las filas haya valores coincidentes o no.</i>
+					</div>
+				</div>
+				<br>
+				<b class = "colorComando">SELECT </b> table1.id, table1.name, table2.total<b class = "colorComentario"> # La columna relacionada, es decir que su valor es compartido entre ambas tablas, es el 'id'. En este caso, muestro las columnas 'id' y 'name' de la 'table1'; y 'total' de la 'table2'.</b><br>
+				<b class = "colorComando">FROM</b> table1<b class = "colorComentario"> # La tabla original, en la que se muestra el campo relacionado.</b><br>
+				<b class = "colorComando">INNER JOIN</b> table2 <b class = "colorComando">ON</b> table1.id = table2.id;<b class = "colorComentario"> # Combino la tabla 2 con la tabla 1 relacionando las columnas id de ambas. Utilizo la clausula 'INNER JOIN'.</b><br>
+				<br>
+				<b class = "colorComando">SELECT </b> table1.id, table1.name, table2.total, table3.regionName<br>
+				<b class = "colorComando">FROM</b> ((table1<br>
+				<b class = "colorComando">INNER JOIN</b> table2 <b class = "colorComando">ON</b> table1.id = table2.id)<br>
+				<b class = "colorComando">INNER JOIN</b> table3 <b class = "colorComando">ON</b> table1.idRegion = table3.id);<b class = "colorComentario"> # Relaciono tres tablas, no necesariamente comparten las 3 el mismo campo relacionado. En este caso la tabla 1 esta relacionada con la 2 y la 3, pero estas no tiene ninguna relacion entre si.</b>
+				<br><br>
+				<hr>
+				<br>
+				<b class = "colorImportante indice" id = "info_union">UNION:</b><i> Une los resultados de dos o mas 'SELECT', siempre y cuando:<br>
+				<ul>
+					<li>Cada 'SELECT' debe tener el mismo numero de columnas.</li>
+					<li>Estas columnas deben tener el mismo tipo o similar.</li>
+					<li>El orden de las columnas debe ser identico.</li>
+				</ul>
+				</i>
+				<b class = "colorComando">SELECT </b> column_name1, column_name2<br>
+				<b class = "colorComando">FROM</b> table_name1<br>
+				<b class = "colorComando">UNION</b><b class = "colorComentario"> # Si existen duplicados no se mostraran.</b><br>
+				<b class = "colorComando">SELECT </b> column_name_a, column_name_b<b class = "colorComentario"> # El nombre de las columnas no importa, si no su contenido.</b><br>
+				<b class = "colorComando">FROM</b> table_name2;<b class = "colorComentario"> # El nombre de las columnas de la tabla resultado sera el del primer 'SELECT'.</b><br>
+				<br><br>
+				<b class = "colorComando">SELECT </b><b class = "colorString">'tabla 1'</b> <b class = "colorComando">AS</b> TIPO, column_name1, column_name2<b class = "colorComentario"> # La columna 'TIPO' no pertenece a ninguna de las dos tablas, sino que es una temporal que tendra dos valores posibles: un string ('tabla 1') que se mostrara en las filas de la tabla 1 y otro ('tabla 2') para las de la tabla 2.</b><br>
+				<b class = "colorComando">FROM</b> table_name1<br>
+				<b class = "colorComando">WHERE</b> condicion<br>
+				<b class = "colorComando">UNION ALL</b><b class = "colorComentario"> # Si existen duplicados ahora los muestra.</b><br>
+				<b class = "colorComando">SELECT </b><b class = "colorString">'tabla 2'</b> <b class = "colorComando">AS</b> TIPO, column_name_a, column_name_b<br>
+				<b class = "colorComando">FROM</b> table_name2<br>
+				<b class = "colorComando">WHERE</b> condicion<br>
+				<b class = "colorComando">ORDER BY</b> column_name1;<br>
+				
+
+
+
 
 
 				</div>
